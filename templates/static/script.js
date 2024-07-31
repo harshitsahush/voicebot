0c0c04def709fcedfaec13f7dfff035928b7992b
harshitsahush <harshitsahu.sh@gmail.com>
2024-07-31 21:01:13 +0530

    Added speech recognition using SpeechRecognition

---
+++ templates/static/script.js
@@ -0,0 +1,47 @@
+// Ensure the script runs after the DOM is fully loaded
+window.onload = () => {
+    // Check for browser compatibility with SpeechRecognition
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    
+    if (SpeechRecognition) {
+        const recognition = new SpeechRecognition();
+        
+        // Recognition settings
+        recognition.continuous = false;
+        recognition.lang = 'en-US';
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+    
+        // Event handler for when the recognition service returns a result
+        recognition.onresult = (event) => {
+            const temp = event.results[0][0].transcript;
+            console.log(temp);
+        };
+
+        // event handler to restart the recogn when it ends
+        // recognition.onend = () => {
+        //     recognition.start();
+        // };
+
+    
+        // Event handler for when the recognition service encounters an error
+        recognition.onerror = (event) => {
+            console.error('Error occurred in recognition: ', event.error);
+        };
+    
+        // Set up click event handler on the body
+        document.querySelector("#start").onclick = () => {
+            console.log("Ready to recognise");
+            recognition.start();
+        };
+
+        // stop voice recognition
+        document.querySelector("#stop").onclick = () => {
+            recognition.stop();
+        }
+    }
+
+    else {
+        console.log('Speech Recognition API not supported in this browser.');
+    }
+}; 
